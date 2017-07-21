532bf750016d53de98f75f2d308c0b35c69b2997
Brian Fan <kuomu.fan@yahoo.com>
2017-07-21 10:22:26 -0700

    use Faker to seed randomized data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_pokemons
+  end
+
+  def generate_pokemons
+    20.times do |i|
+      pokemon = Monster.create!(
+        name: Faker::Pokemon.name,
+        location: Faker::Pokemon.location,
+        move: Faker::Pokemon.move
+      )
+      puts "Pokemon #{i}: You can find #{pokemon.name} in #{pokemon.location}."
+    end
+  end
+end
+
+Seed.begin
