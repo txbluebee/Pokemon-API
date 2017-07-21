391222efb5a499c91fa8621945169d61d21799c6
Brian Fan <kuomu.fan@yahoo.com>
2017-07-21 11:50:08 -0700

    add api visitioning, now data can be requested throught api/v1/monsters

---
+++ app/controllers/api/v1/monsters_controller.rb
@@ -0,0 +1,57 @@
+class Api::V1::MonstersController < ApplicationController
+  before_action :set_monster, only: [:show, :update, :destroy]
+
+  # GET /monsters
+  def index
+    if params[:name]
+      @monsters = Monster.search(params[:name])
+    elsif
+      params[:random_pokemon]
+      @monsters = Monster.random_pokemon
+    else
+      @monsters = Monster.all
+    end
+    json_response(@monsters)
+  end
+
+  # GET /monsters/1
+  def show
+    json_response(@monster)
+  end
+
+  # POST /monsters
+  def create
+    @monster = Monster.create!(monster_params)
+    json_response(@monster, :created)
+  end
+
+  # PATCH/PUT /monsters/1
+  def update
+    if @monster.update!(monster_params)
+      json_response(@monster, :ok)
+    else
+      json_response(@monster, :unprocessable_entity)
+    end
+  end
+
+  # DELETE /monsters/1
+  def destroy
+    @monster.destroy
+    if @monster.destroy
+      head(:ok)
+    else
+      head(:unprocessable_entity)
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_monster
+      @monster = Monster.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def monster_params
+      params.permit(:name, :location, :move)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :monsters
+
+  namespace :api do
+    namespace :v1 do
+      resources :monsters
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
